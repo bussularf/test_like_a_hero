6dc11623b32cf017b6c0309925b5919d168c3e45
Fernanda de Jesus <fernandabussular@gmail.com>
2022-04-15 01:39:15 -0300

    add faker

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,16 +5,17 @@ RSpec.describe User, type: :model do
     nickname = FFaker::Name.first_name
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(100..9999)
-    user = User.new(nickname: nickname, kind: kind, level: level)
 
+    user = User.new(nickname: nickname, kind: kind, level: level)
     expect(user).to_not be_valid
   end
+  
   it "returns the correct hero title" do 
     nickname = FFaker::Name.first_name
     kind = %i[knight wizard].sample
     level = FFaker::Random.rand(1..99)
-    user = User.create(nickname: nickname, kind: kind, level: level)
 
+    user = User.create(nickname: nickname, kind: kind, level: level)
     expect(user.title).to eq( "#{kind} #{nickname} ##{level}" )
   end
 end
